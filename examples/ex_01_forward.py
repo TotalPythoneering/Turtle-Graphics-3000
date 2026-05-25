efc9a9774a5d069048ead97d64a56454559ea17e
Mr. Randall Nagy <randall.nagy@gmail.com>
2026-05-25 13:26:14 -0400

    turtle.exitonclick()
    alternative.

--- examples/ex_01_forward.py
+++ examples/ex_01_forward.py
@@ -1,8 +1,7 @@
 # MISSION: Examples from the Python 3000 educational opportunity.
 # STATUS: Public Release
-# VERSION: 1.0.0
+# VERSION: 1.1.0
 # NOTES: Code: https://github.com/TotalPythoneering/Turtle-Graphics-3000
-# DATE: 2018-08-09 05:18:34
 # FILE: ex_01_forward.py
 # AUTHOR: Videos: https://www.youtube.com/playlist?list=PLb_nLwX4CF6g56CUJEEhb5WiDUhU_sTi4
 #
@@ -13,6 +12,5 @@ import turtle
 # Default "home" orientation
 turtle.forward(100)
 
-turtle.done()
-
-
+# turtle.done()
+turtle.exitonclick()
